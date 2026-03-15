b2916c730d703205aa265be7dbf4f288dacb3be4
mohammed nadeer <mohammednadeer20@gmail.com>
2026-03-15 14:59:06 +0530

    Scoreboard and Game over added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,45 +1,54 @@
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPun
 {
     public static GameManager Instance;
+
+    [Header("Spawn Points")]
     public Transform spawnLeft;
     public Transform spawnRight;
 
     Dictionary<int, int> playerScores = new Dictionary<int, int>();
 
+    [Header("Score UI")]
+    public TextMeshProUGUI player1Score;
+    public TextMeshProUGUI player2Score;
+
+    [Header("Game Over UI")]
+    public GameObject gameOverPanel;
+    public TextMeshProUGUI resultText;
+
+    int winScore = 5;
+
     void Awake()
-{
-    if (Instance == null)
     {
-        Instance = this;
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
     }
-    else
-    {
-        Destroy(gameObject);
-    }
-}
 
     void Start()
     {
         SpawnPlayer();
+
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(false);
     }
 
     void SpawnPlayer()
     {
         Vector3 spawnPos;
 
-        // First player in room
         if (PhotonNetwork.IsMasterClient)
-        {
             spawnPos = spawnLeft.position;
-        }
         else
-        {
             spawnPos = spawnRight.position;
-        }
 
         PhotonNetwork.Instantiate(
             "Player 1",
@@ -48,15 +57,75 @@ public class GameManager : MonoBehaviour
         );
     }
 
+    // Called when a player kills another player
     public void AddKill(int attackerID)
     {
+        photonView.RPC("RPC_AddKill", RpcTarget.All, attackerID);
+    }
+
+    // Runs on ALL clients
+    [PunRPC]
+    void RPC_AddKill(int attackerID)
+    {
         if (!playerScores.ContainsKey(attackerID))
-        {
             playerScores[attackerID] = 0;
-        }
 
         playerScores[attackerID]++;
 
-        Debug.Log("Player " + attackerID + " Score: " + playerScores[attackerID]);
+        UpdateScoreUI();
+
+        CheckWin(attackerID);
+    }
+
+    void UpdateScoreUI()
+    {
+        int p1Score = 0;
+        int p2Score = 0;
+
+        if (playerScores.ContainsKey(1))
+            p1Score = playerScores[1];
+
+        if (playerScores.ContainsKey(2))
+            p2Score = playerScores[2];
+
+        if (player1Score != null)
+            player1Score.text = p1Score.ToString("00");
+
+        if (player2Score != null)
+            player2Score.text = p2Score.ToString("00");
+    }
+
+    void CheckWin(int attackerID)
+    {
+        if (playerScores[attackerID] >= winScore)
+        {
+            ShowGameOver(attackerID);
+        }
+    }
+
+    void ShowGameOver(int winnerID)
+    {
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(true);
+
+        int myID = PhotonNetwork.LocalPlayer.ActorNumber;
+
+        if (myID == winnerID)
+        {
+            resultText.text = "YOU WIN";
+            resultText.color = new Color(0f, 1f, 0.45f); // green
+        }
+        else
+        {
+            resultText.text = "YOU LOSE";
+            resultText.color = new Color(1f, 0.23f, 0.23f); // red
+        }
+    }
+
+    // Button function
+    public void ReturnToMainMenu()
+    {
+        PhotonNetwork.LeaveRoom();
+        SceneManager.LoadScene("HomeScene");
     }
 }
